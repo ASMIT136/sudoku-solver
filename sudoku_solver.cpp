f1331b3d1a4f908ae66473a0fbc883c055933405
ASMIT136 <143455977+ASMIT136@users.noreply.github.com>
2024-10-14 22:27:15 +0530

    Add files via upload

---
+++ sudoku_solver.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isValid(vector<vector<int>> board, int row, int col, int num) {
+   
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == num || board[i][col] == num) {
+            return false;
+        }
+    }
+
+    int startRow = (row / 3) * 3;
+    int startCol = (col / 3) * 3;
+
+    for (int i = startRow; i < startRow + 3; i++) {
+        for (int j = startCol; j < startCol + 3; j++) {
+            if (board[i][j] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool solveSudoku(vector<vector<int>>& board) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (board[i][j] == 0) {
+                for (int num = 1; num <= 9; num++) {
+                    if (isValid(board, i, j, num)) {
+                        board[i][j] = num;
+
+                        if (solveSudoku(board)) {
+                            return true;
+                        } else {
+                            board[i][j] = 0;
+                        }
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void printSudoku(vector<vector<int>>& board) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<int>> board = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+
+    if (solveSudoku(board)) {
+        cout << "Sudoku solved successfully!\n";
+        printSudoku(board);
+    } else {
+        cout << "No solution exists for the given Sudoku puzzle.\n";
+    }
+
+    return 0;
+}
